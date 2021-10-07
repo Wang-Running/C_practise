1c9d651af1bad3e6d01931fed0d1a5652ef2b00c
Wang-Running <1767479196@qq.com>
2021-10-07 22:07:24 +0800

    牛客入门1-10

---
+++ test-lxt 1-10/test-lxt 1-10/test-lxt 1-10.c
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//BC1.
+//#include <stdio.h>
+//int main(){
+//	printf("Practice makes perfect!");
+//}
+
+//BC2.
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%c   %c\n", 'v', 'v');
+//	printf(" %c %c\n", 'v', 'v');
+//	printf("  %c", 'v');
+//	return 0;
+//}
+
+//BC3.
+//#include <stdio.h>
+//int main()
+//{
+//	printf("The size of short is %d bytes.\n", sizeof(short));
+//	printf("The size of int is %d bytes.\n", sizeof(int));
+//	printf("The size of long is %d bytes.\n", sizeof(long));
+//	printf("The size of long long is %d bytes.\n", sizeof(long long));
+//	return 0;
+//}
+
+//BC6.
+//#include <stdio.h>
+//int main()
+//{
+//	printf("     **     \n");
+//	printf("     **     \n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *     \n");
+//	printf("    *  *     \n");
+//
+//	return 0;
+//}
+
+//BC7.
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%#o %#X", 1234, 1234);
+//	return 0;
+//}
+
+//BC8.
+//#include <stdio.h>
+//int main()
+//{
+//	int n = 0xabcdef;
+//	printf("%15d", n);
+//}
+
+//BC9.
+//#include <stdio.h>
+//int main()
+//{
+//	printf("\n%d", printf("Hello world!"));
+//	return 0;
+//}
+
+//BC10.
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d %d %d", &a, &b, &c);
+//	printf("score1=%d,score2=%d,score3=%d", a, b, c);
+//	return 0;
+//}
