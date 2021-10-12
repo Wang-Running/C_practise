8b0737d870db068d206921bfb0c91ed045a9deb6
Wang-Running <1767479196@qq.com>
2021-10-12 21:24:42 +0800

    牛客入门16-20

---
+++ test-lxt 15-20/test-lxt 15-20/test-lxt 15-20.c
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS
+//BC16
+//#include <stdio.h>
+//int main()
+//{
+//	char a;
+//	scanf("%c", &a);
+//	printf("%d", a);
+//	return 0;
+//}
+
+//BC17
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 40;
+//	int c = 212;
+//	int z = (-8 + 22)*a - 10 + c / 2;
+//	printf("%d", z);
+//	return 0;
+//}
+
+//BC18
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int s = 0;
+//	int z = 0;
+//	scanf("%d %d", &a, &b);
+//	s = a / b;
+//	z = a%b;
+//	printf("%d %d", s, z);
+//	return 0;
+//}
+
+//BC19
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int i = 0;
+//	int n = 0;
+//	scanf("%d", &a);
+//	for (i = 0; i<4; i++)
+//	{
+//		n = a % 10;
+//		a = a / 10;
+//		printf("%d", n);
+//	}
+//	return 0;
+//}
+
+
+
+//BC20
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	scanf("%d %d", &a, &b);
+//	sum = a + b;
+//	if (sum > 99)
+//	{
+//		printf("%d", sum % 100);
+//	}
+//	else
+//		printf("%d",sum);
+//	return 0;
+//}
