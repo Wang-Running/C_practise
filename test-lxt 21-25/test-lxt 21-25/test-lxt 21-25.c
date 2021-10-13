7c3c3fc3582144b0fc97ff45217b57488ad32af9
Wang-Running <1767479196@qq.com>
2021-10-13 23:34:23 +0800

    牛客入门21-25

---
+++ test-lxt 21-25/test-lxt 21-25/test-lxt 21-25.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+//BC21
+//#include <stdio.h>
+//int main()
+//{
+//	float a = 0;
+//	int s = 0;
+//	scanf("%f", &a);
+//	s = (int)a;
+//	s = s % 10;
+//	printf("%d", s);
+//	return 0;
+//}
+
+//BC22
+//#include <stdio.h>
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	long int s = 3.156 * 10000000 * age;
+//	printf("%ld", s);
+//	return 0;
+//}
+
+//BC23
+//#include <stdio.h>
+//int main()
+//{
+//	int s = 0;
+//	int m = 0;
+//	int h = 0;
+//	int second = 0;
+//	scanf("%d", &s);
+//	h = s / 3600;
+//	m = s % 3600 / 60;
+//	second = s % 3600 % 60;
+//	printf("%d %d %d", h, m, second);
+//	return 0;
+//}
+
+//BC24
+//#include<stdio.h>
+//int main()
+//{
+//	float a, b, c, sum, pj;
+//	scanf("%f %f %f", &a, &b, &c);
+//	sum = a + b + c;
+//	pj = (a + b + c) / 3;
+//	printf("%.2lf %.2lf", sum, pj);
+//	return 0;
+//}
+
+//BC25
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	int kg = 0;
+//	int cm = 0;
+//	float BMI;
+//	scanf("%d %d", &kg, &cm);
+//	BMI = ((float)kg / pow((float)cm / 100, 2));
+//	printf("%.2f", BMI);
+//	return 0;
+//}
+
+//pow(int x,int y) ÇóxµÄy´ÎÃÝ
