0ad499e6da00123ff2fd7110aca26a03818b7b05
Wang-Running <1767479196@qq.com>
2021-10-18 23:25:29 +0800

    牛客入门36-40

---
+++ test-lxt 36-40/test-lxt 36-40/test-lxt 36-40.c
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS
+//BC36
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	float kg = 0;
+//	float m = 0;
+//	float BIM = 0;
+//	scanf("%f %f", &kg, &m);
+//	BIM = kg / pow(m, 2);
+//	if (18.5<BIM&&BIM<23.9)
+//		printf("Normal");
+//	else
+//		printf("Abnormal");
+//	return 0;
+//}
+
+//BC37
+//#include<stdio.h>
+//int main()
+//{
+//	float jg = 0;
+//	int yf = 0;
+//	int rq = 0;
+//	int yhq = 0;
+//	scanf("%f %d %d %d", &jg, &yf, &rq, &yhq);
+//	if (yf == 11, rq == 11)
+//	{
+//		if (yhq == 0)
+//		{
+//			jg = jg*0.7;
+//			if (jg>0)
+//				printf("%.2f", jg);
+//			else
+//				printf("0.00");
+//		}
+//		if (yhq == 1)
+//		{
+//			jg = jg*0.7 - 50;
+//			if (jg>0)
+//				printf("%.2f", jg);
+//			else
+//				printf("0.00");
+//		}
+//	}
+//	else if (yf == 12, rq == 12)
+//	{
+//		if (yhq == 0)
+//		{
+//			jg = jg*0.8;
+//			if (jg>0)
+//				printf("%.2f", jg);
+//			else
+//				printf("0.00");
+//		}
+//		if (yhq == 1)
+//		{
+//			jg = jg*0.8 - 50;
+//			if (jg>0)
+//				printf("%.2f", jg);
+//			else
+//				printf("0.00");
+//		}
+//	}
+//	else
+//		printf("%.2f", jg);
+//	return 0;
+//}
+
+//BC38
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 10000; i<100000; i++)
+//	{
+//		if (i == (i / 10000)*(i % 10000) + (i / 1000)*(i % 1000) + (i / 100)*(i % 100) + (i / 10)*(i % 10))
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//BC39
