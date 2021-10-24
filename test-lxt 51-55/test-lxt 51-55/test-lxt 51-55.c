57928dadf96c44e55e37d2e94e6046cd1d5e8953
Wang-Running <1767479196@qq.com>
2021-10-24 20:42:17 +0800

    牛客入门 51-55

---
+++ test-lxt 51-55/test-lxt 51-55/test-lxt 51-55.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+//BC51
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	while (scanf("%d %d %d ", &a, &b, &c) != EOF)
+//	{
+//		if (a + b>c && a + c>b && b + c>a)//判断是不是三角形
+//		{
+//			if (a == b || a == c || b == c)//判断是不是等腰三角形
+//			{
+//				if (a == b && a == c)//判断是不是等边三角形
+//				{
+//					printf("Equilateral triangle!\n");
+//				}
+//				else
+//				{
+//					printf("Isosceles triangle!\n");
+//				}
+//			}
+//			else
+//			{
+//				printf("Ordinary triangle!\n");
+//			}
+//		}
+//		else
+//		{
+//			printf("Not a triangle!\n");
+//		}
+//	}
+//	return 0;
+//}
+
+//BC52
+//#include <stdio.h>
+//int main()
+//#define b w/((h/100)*(h/100))
+//{
+//	float w, h = 0;
+//	while (scanf("%f %f", &w, &h) != EOF)
+//	{
+//		if (b<18.5)
+//			printf("Underweight\n");
+//		else if (b >= 18.5&&b <= 23.9)
+//			printf("Normal\n");
+//		else if (b>27.9)
+//			printf("Obese\n");
+//		else
+//			printf("Overweight\n");
+//	}
+//	return 0;
+//}
+
+//BC53
