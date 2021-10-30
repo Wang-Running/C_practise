692a7ee48e81b830487e27ee99b1df26e72a7555
Wang-Running <1767479196@qq.com>
2021-10-30 23:32:03 +0800

    牛客入门 51-55

--- test-lxt 51-55/test-lxt 51-55/test-lxt 51-55.c
+++ test-lxt 51-55/test-lxt 51-55/test-lxt 51-55.c
@@ -55,3 +55,47 @@
 //}
 
 //BC53
+
+
+//BC54
+//首先要清楚月份天数的分类
+//每年的4，6，9，11月的天数都是30天
+//每年的1，3，5，7，8，10，12月都是31天
+//然后计算2月的天数
+//2月的天数有两种，闰年为29天，非闰年28天
+#include <stdio.h>
+int main()
+{
+	int year, month, day;
+	while (scanf("%d %d", &year, &month) != EOF)
+	{
+		switch (month)
+		{
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			printf("31\n");
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			printf("30\n");
+			break;
+		default:
+			if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+			{
+				printf("29\n");
+			}
+			else
+			{
+				printf("28\n");
+			}
+		}
+	}
+	return 0;
+}
